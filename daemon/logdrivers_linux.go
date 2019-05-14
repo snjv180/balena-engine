bd602266f6e60f7aff51f15165b47b2a73b8329d
sshrestha <sshrestha@cradlepoint.com>
2019-05-14 12:22:55 -0600

    Enable Syslog instead of journald
    Need to use syslog instead of journald.

--- daemon/logdrivers_linux.go
+++ daemon/logdrivers_linux.go
@@ -3,6 +3,6 @@ package daemon // import "github.com/docker/docker/daemon"
 import (
 	// Importing packages here only to make sure their init gets called and
 	// therefore they register themselves to the logdriver factory.
-	_ "github.com/docker/docker/daemon/logger/journald"
+	_ "github.com/docker/docker/daemon/logger/syslog"
 	_ "github.com/docker/docker/daemon/logger/jsonfilelog"
 )
